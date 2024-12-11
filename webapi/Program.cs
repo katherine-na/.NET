0d2452e7e3122e739cb43764d679df8339cf61c7
Katherine Negrete <katherine.negrete@drsecurity.net>
2024-12-11 13:19:55 -0600

    First commit .NET Fundamentals

---
+++ webapi/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Por favor ingresa un nombre");
+var nombre = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su edad");
+var edad = Console.ReadLine();
+
+Console.WriteLine($"Hola mi nombre es {nombre}, mi cargo es {cargo} y tengo {edad} años");
